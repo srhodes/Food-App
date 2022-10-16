683c6055259e5aa589ac8cd5f7fc86c926e32356
srhodes <srhodes@DESKTOP-UV6CR3J.localdomain>
2022-10-16 13:39:27 -0700

    Try a searching algorithm for adding food

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, render_template, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 # from flask_login import UserMixin
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, FloatField
 from wtforms.validators import InputRequired, Length, ValidationError
 from models import db, connect_db, User
 from secrets import API_SECRET_KEY
@@ -23,6 +23,12 @@ class loginForm(FlaskForm):
     username = StringField("Username")
     password = PasswordField("Password")
 
+class AddFoodInfo(FlaskForm):
+    name = StringField("Food Name")
+    calories = FloatField("Calories")
+    carbs = FloatField("carbs")
+    fat = FloatField("fat")
+    protein = FloatField("protein")
 
 @app.route('/home')
 def index():
@@ -70,6 +76,202 @@ def index():
 
     # data2 = res2.json()
     return render_template("index.html", name1 = data1['image'], name2 =data2['image'] , name3=data3['image'])
+
+@app.route('/price')
+def price():
+    # Document at https://spoonacular.com/food-api/docs#Get-Recipe-Information
+
+    res = requests.get('https://api.spoonacular.com/recipes/716429/information?apiKey=d7c35df411774b8abdc4c5197ab01533')
+    # https://api.spoonacular.com/recipes/716429/information?apiKey=d7c35df411774b8abdc4c5197ab01533&includeNutrition=true
+    # POST Request - https://spoonacular.com/food-api/docs#Create-Recipe-Card
+    # GET Request - https://spoonacular.com/food-api/docs#Get-Recipe-Card
+
+    # https://api.spoonacular.com/mealplanner/generate?timeFrame=day,targetCalories=2000&apiKey=d7c35df411774b8abdc4c5197ab01533 
+
+    # res = requests.get('https://api.spoonacular.com/recipes/716429/information', params={'apiKey': API_SECRET_KEY, 'includeNutrition': True})
+    
+    res = requests.get('https://api.spoonacular.com/mealplanner/generate', params={'apiKey': API_SECRET_KEY, 'timeFrame':'day', 'targetCalories': 2000})
+
+
+    data = res.json()
+    # img = data['image']
+
+    # obj = {'img': img}
+    firstMealID= data['meals'][0]['id']
+    secondMealID = data['meals'][1]['id']
+    thirdMealID = data['meals'][2]['id']
+    mealList = [firstMealID, secondMealID, thirdMealID]
+
+    # for meal in mealList
+    # print(meal)
+    #
+    
+    
+    res1 = requests.get(f'https://api.spoonacular.com/recipes/{firstMealID}/information', params={'apiKey': API_SECRET_KEY, 'includeNutrition': True})
+    res2 = requests.get(f'https://api.spoonacular.com/recipes/{secondMealID}/information', params={'apiKey': API_SECRET_KEY, 'includeNutrition': True})
+    res3 = requests.get(f'https://api.spoonacular.com/recipes/{thirdMealID}/information', params={'apiKey': API_SECRET_KEY, 'includeNutrition': True})
+
+    data1 = res1.json() 
+    data2 = res2.json() 
+    data3 = res3.json() 
+    resList = [data1['image'], data2['image'], data3['image']]
+
+
+    # for meal in mealList:
+    #     print(meal)
+    #     resList.append(meal) 
+    #     print(resList)
+
+    # data2 = res2.json()
+    return render_template("price.html", 
+    name1 = data1['image'], 
+    name2 = data2['image'] , 
+    name3 = data3['image'], 
+    diet1 = data1['diets'],
+    diet2 = data2['diets'],
+    diet3 = data3['diets'],
+    inst1 = data1['instructions'],
+    inst2 = data2['instructions'],
+    inst3 = data3['instructions']
+    )
+
+@app.route('/nutrients', methods=["GET", "POST"])
+def nutrients():
+    # Document at https://spoonacular.com/food-api/docs#Get-Recipe-Information
+
+    # res = requests.get('https://api.spoonacular.com/recipes/findByNutrients?minCarbs=10&maxCarbs=50&number=2&apiKey=d7c35df411774b8abdc4c5197ab01533')
+    # https://api.spoonacular.com/recipes/716429/information?apiKey=d7c35df411774b8abdc4c5197ab01533&includeNutrition=true
+    # POST Request - https://spoonacular.com/food-api/docs#Create-Recipe-Card
+    # GET Request - https://spoonacular.com/food-api/docs#Get-Recipe-Card
+
+    # https://api.spoonacular.com/mealplanner/generate?timeFrame=day,targetCalories=2000&apiKey=d7c35df411774b8abdc4c5197ab01533 
+
+    # res = requests.get('https://api.spoonacular.com/recipes/716429/information', params={'apiKey': API_SECRET_KEY, 'includeNutrition': True})
+    
+    form = AddFoodInfo()
+    res = requests.get('https://api.spoonacular.com/recipes/findByNutrients', params={'apiKey': API_SECRET_KEY, 'minCarbs':10, 'maxCarbs': 50, 'number': 2})
+
+
+    data = res.json()
+    # img = data['image']
+
+    # obj = {'img': img}
+    firstMealID= data[0]['id']
+    firstMealCalories = data[0]['calories']
+    firstMealProtein = data[0]['protein']
+    firstMealFat = data[0]['fat']
+
+    secondMealID = data[1]['id']
+    secondMealCalories = data[1]['calories']
+    secondMealProtein = data[1]['protein']
+    secondMealFat = data[1]['fat']
+
+    
+    mealList = [firstMealID, secondMealID]
+
+    # for meal in mealList
+    # print(meal)
+    #
+    
+    
+    res1 = requests.get(f'https://api.spoonacular.com/recipes/{firstMealID}/information', params={'apiKey': API_SECRET_KEY, 'includeNutrition': True})
+    res2 = requests.get(f'https://api.spoonacular.com/recipes/{secondMealID}/information', params={'apiKey': API_SECRET_KEY, 'includeNutrition': True})
+
+
+    data1 = res1.json() 
+    data2 = res2.json() 
+
+    resList = [data1['image'], data2['image']]
+
+
+    # for meal in mealList:
+    #     print(meal)
+    #     resList.append(meal) 
+    #     print(resList)
+
+    # data2 = res2.json()
+    if form.validate_on_submit():
+        name = form.name.data
+        calories = form.calories.data
+        carbs = form.calories.data
+        fat = form.fat.data
+        protein = form.protein.data
+
+        if name == firstMealTitle:
+            return render_template("nutrients.html",
+            firstMealImage = data1['image'],
+            firstMealTitle = data[0]['title'],
+            secondMealTitle = data[1]['title'], 
+            secondMealImage = data2['image'], 
+            firstMealCalories = data[0]['calories'],
+            firstMealProtein = data[0]['protein'],
+            firstMealFat = data[0]['fat'],
+            firstMealCarbs = data[0]['carbs'],
+            secondMealCalories = data[1]['calories'],
+            secondMealProtein = data[1]['protein'],
+            secondMealFat = data[1]['fat'],
+            secondMealCarbs = data[1]['carbs'],
+            form=form
+            )
+
+        if calories == firstMealCalories or calories == secondMealCalories:
+            return render_template("nutrients.html", 
+             firstMealImage = data1['image'],
+             firstMealTitle = data[0]['title'],
+             firstMealCalories = data[0]['calories'],
+             secondMealTitle = data[1]['title'], 
+             secondMealImage = data2['image'],
+             secondMealCalories = data[1]['calories'],
+             form=form 
+             )
+        if carbs == firstMealCarbs or carbs == secondMealCarbs:
+            return render_template("nutrients.html", 
+             firstMealImage = data1['image'],
+             firstMealTitle = data[0]['title'],
+             firstMealCarbs = data[0]['carbs'],
+             secondMealTitle = data[1]['title'], 
+             secondMealImage = data2['image'],
+             secondMealCarbs = data[1]['carbs'],
+             form=form 
+             )
+        if fat == firstMealFat or fat == secondMealFat:
+            return render_template("nutrients.html", 
+             firstMealImage = data1['image'],
+             firstMealTitle = data[0]['title'],
+             firstMealFat = data[0]['fat'],
+             secondMealTitle = data[1]['title'], 
+             secondMealImage = data2['image'],
+             secondMealFat = data[1]['fat'],
+             form=form 
+             )
+        if protein == firstMealProtein or protein == secondMealProtein:
+            return render_template("nutrients.html", 
+             firstMealImage = data1['image'],
+             firstMealTitle = data[0]['title'],
+             firstMealProtein = data[0]['protein'],
+             secondMealTitle = data[1]['title'], 
+             secondMealImage = data2['image'],
+             secondMealProtein = data[1]['protein'],
+             form=form 
+             )
+        else:
+            redirect("nutritents.html")
+
+    return render_template("nutrients.html", 
+    # firstMealImage = data1['image'],
+    # firstMealTitle = data[0]['title'],
+    # secondMealTitle = data[1]['title'], 
+    # secondMealImage = data2['image'], 
+    # firstMealCalories = data[0]['calories'],
+    # firstMealProtein = data[0]['protein'],
+    # firstMealFat = data[0]['fat'],
+    # firstMealCarbs = data[0]['carbs'],
+    # secondMealCalories = data[1]['calories'],
+    # secondMealProtein = data[1]['protein'],
+    # secondMealFat = data[1]['fat'],
+    # secondMealCarbs = data[1]['carbs'],
+    form=form
+    )
 
 @app.route('/register', methods=['GET', 'POST'])
 def register_user():
